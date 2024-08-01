724344001270840be800e6871f503a5391bc77cb
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-08-01 13:47:41 +0700

    add product

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {useForm, SubmitHandler} from 'react-hook-form'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
@@ -9,35 +8,6 @@ import ProductManagement from './pages/ProductManagement'
 import AddProduct from './pages/AddProduct'
 
 function App() {
-  // const [products, setProduct] = useState([])
-  // useEffect(()=>{
-  //   const getData = async()=>{
-  //     const response = await fetch("http://localhost:3000/products")
-  //     const data = await response.json()
-  //     setProduct(data)
-  //   }
-  //   getData()
-  // },[])
-
-  
-
-  // const {register, handleSubmit, reset } = useForm()
-  // const onHandleSubmit = async(data) => {
-  //   try {
-  //     await fetch("http://localhost:3000/products",{
-  //       method: "POST",
-  //       headers:{
-  //         "Content-Type":"application/json"
-  //       },
-  //       body: JSON.stringify(data)
-  //     })
-  //     const newProductList = [...products, data]
-  //     setProduct(newProductList)
-  //     reset()
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
 
   const [products,setProducts] = useState([])
   
@@ -60,12 +30,24 @@ function App() {
     }
   }
 
+  
+  const onHandleSubmit = (data) => {
+    fetch("http://localhost:3000/products",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(data)
+    })
+    
+  }
+
   return (
     <>
       <Routes>
         <Route path='/admin' element={<Dashboard/>}/>
         <Route path='/admin/product' element={<ProductManagement products={products} onHandleRemove={onHandleRemove}/>}/>
-        <Route path='/admin/product/add' element={<AddProduct/>}/>
+        <Route path='/admin/product/add' element={<AddProduct onHandleSubmit={onHandleSubmit}/>}/>
       </Routes>
     </>
   )
--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -1,8 +1,19 @@
-const AddProduct = () => {
+import {useForm, SubmitHandler} from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+const AddProduct = (props) => {
+    const {onHandleSubmit} = props
+    
+    const navigate = useNavigate()
+    const {register, handleSubmit, reset } = useForm()
+    const onAdd = (data) => {
+        onHandleSubmit(data)
+        navigate("/admin/product")
+    }
   return (
     <div>
-        <form action="">
-            <input type="text" placeholder='Enter Product Name'/>
+        <form action="" onSubmit={handleSubmit(onAdd)}>
+            <input type="text" placeholder='Enter Product Name' {...register("name")}/>
             <button type="submit">Add Product</button>
         </form>
     </div>
